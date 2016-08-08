0b2c152258350dc09198315bb0a65ec621a0384a
Kauflin, Michael <michael.kauflin@sonova.com>
2016-08-08 23:51:54 +0200

    run tests

--- MatrixBuild/MatrixBuild.Tests/TestFixture_1.cs
+++ MatrixBuild/MatrixBuild.Tests/TestFixture_1.cs
@@ -11,7 +11,7 @@ namespace MatrixBuild.Tests
    internal sealed class TestFixture_1
     {
         [Category("A")]
-        [Test]
+        [TestCase(Result = true)]
         public bool IsTrueTest()
         {
             return true;
