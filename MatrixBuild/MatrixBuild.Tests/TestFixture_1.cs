3dc4f26aecf4ba2ebc247fb097281b1c3a8c157b
Kauflin, Michael <michael.kauflin@sonova.com>
2016-08-08 22:50:33 +0200

    add content

---
+++ MatrixBuild/MatrixBuild.Tests/TestFixture_1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MatrixBuild.Tests
+{
+    [TestFixture]
+   internal sealed class TestFixture_1
+    {
+        [Category("A")]
+        public bool IsTrueTest()
+        {
+            return true;
+        }
+
+        [Category("B")]
+        [TestCase(Result = 0)]
+        public int GetIntTest()
+        {
+            return 0;
+        }
+    }
+}
